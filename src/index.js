44d3ee04c45e481dcb11e6f850dffcb0753768ad
ChrisSyrnyk <christopher.syrnyk2@ucalgary.ca>
2023-12-10 16:38:42 -0700

    added requried packages. imported models

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 require('dotenv/config');
 const express = require('express');
 const cors = require('cors');
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
-const models = require('./models');
+//import models
+const BlogPosts = require('./models/blog_post_model');
+const Comments = require('./models/comment_model');
+const Users = require('./models/user_model');
+//import routes
 const routes = require('./routes');
 
+//setup app
 const app = express();
-
+//setup cors
 app.use(cors());
-
+//setup json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
