d73abd00f3bf7dca04b66311c6b1a54f2ad1b778
ChrisSyrnyk <christopher.syrnyk2@ucalgary.ca>
2023-12-12 15:06:21 -0700

    removed old code

--- src/index.js
+++ src/index.js
@@ -35,49 +35,6 @@ app.use('/users', UserRoutes);
 app.use('/comments', CommentRoutes);
 app.use('/blogposts', BlogPostRoutes);
 
-/*
-//setup security
-app.use(session({ secret: process.env.SESSION_KEY, resave: false, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(express.urlencoded({ extended: false }));
-
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const user = await User.findOne({ username: username });
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      };
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) {
-      // passwords do not match!
-      return done(null, false, { message: "Incorrect password" })
-      }
-      return done(null, user);
-    } catch(err) {
-      return done(err);
-    };
-  })
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch(err) {
-    done(err);
-  };
-});
-*/
-
-
-
-
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
 );
